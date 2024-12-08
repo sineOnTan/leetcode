ff70590b2584c5cbb4e9044870f43e9ddca357fc
Simon <root@SimonPC>
2024-12-08 21:43:47 +1100

    2054 leetcode

--- 2054.cpp
+++ 2054.cpp
@@ -1,19 +1,35 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-#define int long long
 
 #define YES {cout << "YES" << endl;}
 #define NO {cout << "NO" << endl;}
+#define space << " "
 
 class Solution {
 public:
     int maxTwoEvents(vector<vector<int>>& events) {
-        priority_queue<pair<int,int>> endTimes;
-        priority_queue<pair<int,int>> startTimes;
+        set<pair<int,int>> end_time;
+        set<pair<int,int>> start_time;
 
         for (auto i : events) {
-            endTimes.emplace(i[2],i[3]);
+            start_time.emplace(i[0],i[2]);
+            end_time.emplace(i[1],i[2]);
         }
+        int max_val = 0;
+        int max_pred = 0;
+        auto start_p = start_time.begin();
+        auto end_p = end_time.begin();
+
+        while (start_p != start_time.end()){
+            while(end_p != end_time.end() and (*start_p).first > (*end_p).first) {
+                max_pred = max(max_pred, (*end_p).second);
+                end_p = next(end_p);
+            }
+            max_val = max(max_pred + (*start_p).second, max_val);
+            cout << max_val space << max_pred space << (*start_p).second << endl;
+            start_p = next(start_p);
+        }
+        return max_val;
     }
 };
