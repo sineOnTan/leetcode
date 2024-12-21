867285b80d3f7550e9d5e900717eccbcf74474d7
Simon <root@SimonPC>
2024-12-22 00:01:24 +1100

    simplification

--- 2872.cpp
+++ 2872.cpp
@@ -4,20 +4,17 @@ using namespace std;
 
 const long long N = 30010;
 
-
 class Solution {
 public:
     vector<long long> graph_edges[N];
     long long res = 0;
     vector<int> vals;
-    vector<int> children[N];
     long long div;
 
     long long dfs(long long n, long long from = -1) {
         long long temp = vals[n];
         for (auto i : graph_edges[n]) {
             if (i != from) {
-                children[n].push_back(i);
                 temp += dfs(i, n);
             }
         }
@@ -28,11 +25,8 @@ public:
     int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
         vals = values;
         div = k;
-        for (long long i = 0; i < N; i++) {
-            graph_edges[i].clear();
-        }
 
-        for (auto i : edges) {
+        for (auto &i : edges) {
             graph_edges[i[0]].push_back(i[1]);
             graph_edges[i[1]].push_back(i[0]);
         }
