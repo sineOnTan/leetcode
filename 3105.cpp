066b8fd88b72a6e0048a33f31a326f3e0ec6a467
Simon <root@SimonPC>
2025-02-04 04:14:09 +1100

    leet

---
+++ 3105.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& nums) {
+        bool increasing = true;
+        int res = 0;
+        int cur_streak = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > nums[i - 1]) {if(increasing) cur_streak++; else {res = max(cur_streak, res); cur_streak = 1;} increasing = true;}
+            else if (nums[i] > nums[i - 1]) {if(!increasing) cur_streak++; else {res = max(cur_streak, res); cur_streak = 1;} increasing = false;}
+            else {res = max(res, cur_streak); cur_streak = 1;}
+        }
+        return res;
+    }
+};
