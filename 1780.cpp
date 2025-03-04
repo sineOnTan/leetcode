c759430625f5bee1555b54bcc0d6198256529d2c
Simon kimi no na wa <root@Simonlaptop>
2025-03-04 16:32:46 +1100

    t

--- 1780.cpp
+++ 1780.cpp
@@ -5,8 +5,9 @@ class Solution {
     public:
         bool checkPowersOfThree(int n) {
             while (n != 0) {
-                if (n % 3 != 0 or n % 3 != 1) return false;
+                if (n % 3 != 0 and n % 3 != 1) return false;
                 n /= 3;
             }
+            return true;
         }
     };
