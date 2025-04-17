ffeeb98c2d46b81834f611d98920eef9d2b11615
Simon <root@SimonPC>
2025-04-18 01:04:33 +1000

    leet
    '

---
+++ 2537.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        long long countGood(vector<int>& nums, int k) {
+            map<int,int> m;
+
+            for (auto i : nums) m[i]++;
+        }
+    };
