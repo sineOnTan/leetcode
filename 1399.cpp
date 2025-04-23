23e84d81ddddfa649e0376da7393d236089bd761
Simon <root@SimonPC>
2025-04-23 11:31:16 +1000

    leet

--- 1399.cpp
+++ 1399.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Solution {
     public:
         int countLargestGroup(int n) {
-            const int N = 11;
+            const int N = 1e4;
             int a[N];
             for (int i = 0; i < N; i++) a[i] = 0;
 
@@ -12,16 +12,15 @@ class Solution {
                 int temp = i;
                 int count = 0;
                 while (temp != 0) {
-                    count += temp % i;
+                    count += temp % 10;
                     temp /= 10;
                 }
                 a[count]++;
             }
-            
+
             int res = 0;
             int max = 0;
             for (int i = 0; i < N; i++) {
-                cout << a[i] << " ";
                 if (a[i] > max) {
                     res = 1;
                     max = a[i];
