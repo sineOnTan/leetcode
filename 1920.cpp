77e85509029dd957477a4a3d9b9db6f05be85c5f
Simon <root@SimonPC>
2025-05-06 16:43:31 +1000

    leet

---
+++ 1920.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        vector<int> buildArray(vector<int>& nums) {
+            vector<int> res;
+            for (int i = 0; i < nums.size(); i++) {
+                res.push_back
+            }
+        }
+    };
