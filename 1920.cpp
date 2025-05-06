526056eeacf9abed4b6671d4ef611498b4eae179
Simon <root@SimonPC>
2025-05-06 16:53:56 +1000

    leet' git push

--- 1920.cpp
+++ 1920.cpp
@@ -6,7 +6,8 @@ class Solution {
         vector<int> buildArray(vector<int>& nums) {
             vector<int> res;
             for (int i = 0; i < nums.size(); i++) {
-                res.push_back
+                res.push_back(nums[nums[i]]);
             }
+            return res;
         }
     };
