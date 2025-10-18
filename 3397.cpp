b2fdfc113539eb1a5fa4a473955a3797a074f802
Simon <slytan@gmail.com>
2025-10-19 00:42:17 +1100

    leet

---
+++ 3397.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxDistinctElements(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int res = 0;
+        int min = -k;
+
+        for (auto i : nums) {
+            if (min < i - k) {
+                res++;
+                min = i - k;
+            } else if (min < i + k) {
+                res++;
+                min++;
+            }
+        }
+    }
+};
