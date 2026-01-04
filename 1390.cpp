b31e1bd9d2d5ed6da4bd63013969e278f16c701c
Simon <slytan@gmail.com>
2026-01-04 20:12:09 +1100

    leet

---
+++ 1390.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int res = 0;
+
+        for (auto i : nums) {
+            int count_nums = 0;
+            int sum = 0;
+
+            for (int j = 1; j * j < i ; j++) {
+                if (i % j == 0) {
+                    count_nums += 2;
+                    sum += j;
+                    sum += i / j;
+                }
+            }
+            if (count_nums == 4) res += sum;
+        }
+
+        return res;
+    }
+};
